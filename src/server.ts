d564dfd247624bd3648b6f3416b240c11d89680c
Noé Rivera <tsnoe.dali@gmail.com>
2023-07-07 21:11:44 -0600

    Add packages
    A

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,17 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+require("dotenv").config();
 
-//Define your API endpo     ints here
+//Define your API endpoints here
 
 app.listen(port, () => {
   console.log("Starting server...");
   console.log("Server running on port " + port);
   console.log("Press CTRL + C to quit");
+  
+
+
+  console.log("Client " + process.env.TWITCHAPP_CLIENT);
 });
     
