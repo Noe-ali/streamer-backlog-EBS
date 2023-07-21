3e9c84993edeb7c66bca5431f30160c25fee5bbe
Noé Rivera <tsnoe.dali@gmail.com>
2023-07-21 09:03:47 -0600

    strange query

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 require("dotenv").config();
-import { getAuthToken } from "./Auth/tokenRetriever";
+import { AuthToken, getAuthToken } from "./Auth/tokenRetriever";
 
 //test connection
 
-import { Apicalypse } from "apicalypse";
+import apicalypse from "apicalypse";
 const rawQueryString =
   "fields name, summary, rating, platforms.name, cover.url;";
 
@@ -19,21 +19,50 @@ app.listen(port, async () => {
 
   console.log("To test the connection, here's a sample request");
   console.log("Requesting auth token first ");
-   await getAuthToken().then((token) => {
-    console.log('retrieve same token')
-     getAuthToken();
-  });
+  const token: AuthToken = await getAuthToken();
+  await searchZelda(token);
+  await searchSpecificZelda(token);
 });
 
-// async function searchZelda(): Promise<void>{
-//   try {
-//     const igdbClient = new Apicalypse({
-//       baseUrl: "https://api.igdb.com/v4",
-//       headers: {
-//         'Client-ID': process.env.TWITCHAPP_CLIENT,
-//         'Authorization': `Bearer ${}`,
-//       }
+async function searchZelda(token: AuthToken): Promise<void> {
+  console.log("searching for zelda games");
 
-//     })
-//   }
-// }
+  console.log("token is " + token.access_token);
+  const response = await apicalypse({
+    queryMethod: "body",
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Client-ID": process.env.TWITCHAPP_CLIENT,
+      Authorization: `Bearer ${token.access_token}`,
+    },
+  })
+    .fields(["name", "summary", "summary", "platforms.name", "cover.url"])
+    .where([
+      'franchises.name = "The Legend of Zelda"',
+      'themes.name !~ "Compilation"',
+      'themes.name !~ "Anthology"',
+    ])
+    .request("https://api.igdb.com/v4/games");
+
+  console.log(response.data);
+}
+
+async function searchSpecificZelda(token: AuthToken): Promise<void> {
+  console.log("searching for Zelda Wind Waker");
+
+  const response = await apicalypse({
+    queryMethod: "body",
+    method: "POST",
+    headers: {
+      accept: "application/json",
+      "Client-ID": process.env.TWITCHAPP_CLIENT,
+      Authorization: `Bearer ${token.access_token}`,
+    },
+  })
+    .fields(["name", "summary", "summary", "platforms.name", "cover.url"])
+    .where(["id = 1033"])
+    .request("https://api.igdb.com/v4/games");
+
+  console.log(response.data);
+}
